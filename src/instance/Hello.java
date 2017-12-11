76fb775e28e7f043bf08092358ae737274bf21be
paul <paulkapay@gmail.com>
2017-12-11 23:34:56 +0300

    first commit

---
+++ src/instance/Hello.java
@@ -0,0 +1,9 @@
+package instance;
+
+public class Hello {
+    static public void main(String[] args){
+        System.out.println("hello");
+        System.out.println("hello!!!");
+        System.out.println("hello!!!");
+    }
+}
